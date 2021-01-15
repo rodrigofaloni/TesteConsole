ece4e34d1c302308d9eb64618db687df63b0c5ae
Rodrigo Faloni <rodrigofaloni@outlook.com>
2021-01-15 20:45:58 -0300

    Subindo a aplicação.

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    /// <summary>
+    /// Classe principal.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Define o ponto de início da aplicação.
+        /// </summary>
+        /// <param name="args">Os argumentos.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exemplo de uso!");
+            Console.ReadKey();
+        }
+    }
+}
